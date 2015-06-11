4499358434de82b128e23593d7a95240f59b9f60
uncleF <uncle.funkay@gmail.com>
2015-06-11 05:40:44 +0300

    Project Update

--- gruntfile.js
+++ gruntfile.js
@@ -1,113 +1,122 @@
 //Gruntfile for the Slideroony Project
 
-var BUILD_DIR					= "src",							// Project Build
-		DEVELOPMENT_DIR		= "dev",								// Project Development
-		DEMO_DIR 					= "demo",								// Project Demo
-		JS_FILENAME				= "slideroony";					// Production JavaScript Filename
+var BUILD_DIR         = 'src',          // Project Build
+    DEVELOPMENT_DIR   = 'dev',          // Project Development
+    DEMO_DIR          = 'demo',         // Project Demo
+    JS_FILENAME       = 'swipeoroony';  // Production JavaScript Filename
 
 module.exports = function(grunt) {
 
-	var project = {
-		init: function() {
-			this.dir = DEVELOPMENT_DIR + "/";
-			this.build = BUILD_DIR + "/";
-			this.demo = DEMO_DIR + "/";
-			this.filename = JS_FILENAME;
-			return this;
-		}
-	}.init();
+  var project = {
+    init: function() {
+      this.dir = DEVELOPMENT_DIR + '/';
+      this.build = BUILD_DIR + '/';
+      this.demo = DEMO_DIR + '/';
+      this.filename = JS_FILENAME;
+      return this;
+    }
+  }.init();
 
-	require("load-grunt-tasks")(grunt);
+  require('load-grunt-tasks')(grunt);
 
-	grunt.initConfig({
+  grunt.initConfig({
 
-		jshint: {
-			options: {
-				"jshintrc": ".jshintrc"
-			},
-			jsHint: {
-				cwd: project.dir,
-				src: ["*.js"],
-				expand: true
-			}
-		},
-		jsinspect: {
-			jsInspect: {
-				cwd: project.dir,
-				src: ["*.js"],
-				expand: true
-			}
-		},
+    jscs: {
+      options: {
+        config: '.jscsrc'
+      },
+      jscs: {
+        cwd: project.dir,
+        src: ['*.js'],
+        expand: true
+      }
+    },
+    jshint: {
+      options: {
+        'jshintrc': '.jshintrc'
+      },
+      jsHint: {
+        cwd: project.dir,
+        src: ['*.js'],
+        expand: true
+      }
+    },
+    jsinspect: {
+      jsInspect: {
+        cwd: project.dir,
+        src: ['*.js'],
+        expand: true
+      }
+    },
 
-		removelogging: {
-			jsClean: {
-				cwd: project.build,
-				src: ["*.js"],
-				dest: project.build,
-				expand: true
-			}
-		},
-		fixmyjs: {
-			options: {
-				config: ".jshintrc",
-				indentpref: "tabs"
-			},
-			fixMyJS: {
-				cwd: project.build,
-				src: ["*.js"],
-				dest: project.build,
-				expand: true
-			}
-		},
-		uglify: {
-			options: {
-				preserveComments: false
-			},
-			jsMin: {
-				cwd: project.build,
-				src: ["*.js"],
-				dest: project.build,
-				ext: ".min.js",
-				expand: true
-			}
-		},
+    removelogging: {
+      jsClean: {
+        cwd: project.build,
+        src: ['*.js'],
+        dest: project.build,
+        expand: true
+      }
+    },
+    fixmyjs: {
+      options: {
+        config: '.jshintrc'
+      },
+      fixMyJS: {
+        cwd: project.build,
+        src: ['*.js'],
+        dest: project.build,
+        expand: true
+      }
+    },
+    uglify: {
+      options: {
+        preserveComments: false
+      },
+      jsMin: {
+        cwd: project.build,
+        src: ['*.js'],
+        dest: project.build,
+        ext: '.min.js',
+        expand: true
+      }
+    },
 
-		clean: {
-			build: [project.build]
-		},
-		copy: {
-			build: {
-				cwd: project.dir,
-				src: ["**/*.js"],
-				dest: project.build,
-				expand: true
-			},
-			demo: {
-				cwd: project.build,
-				src: ["*.min.js"],
-				dest: project.demo,
-				expand: true
-			}
-		},
-		compress: {
-			jsGzip: {
-				options: {
-					mode: "gzip"
-				},
-				cwd: project.build,
-				src: ["*.min.js"],
-				dest: project.build,
-				ext: ".min.js.gz",
-				expand: true
-			}
-		}
+    clean: {
+      build: [project.build]
+    },
+    copy: {
+      build: {
+        cwd: project.dir,
+        src: ['**/*.js'],
+        dest: project.build,
+        expand: true
+      },
+      demo: {
+        cwd: project.build,
+        src: ['*.min.js'],
+        dest: project.demo,
+        expand: true
+      }
+    },
+    compress: {
+      jsGzip: {
+        options: {
+          mode: 'gzip'
+        },
+        cwd: project.build,
+        src: ['*.min.js'],
+        dest: project.build,
+        ext: '.min.js.gz',
+        expand: true
+      }
+    }
 
-	});
+  });
 
-	grunt.registerTask("quality", ["jshint", "jsinspect"]);
+  grunt.registerTask('quality', ['jscs', 'jshint', 'jsinspect']);
 
-	grunt.registerTask("compile", ["removelogging", "fixmyjs", "uglify"]);
+  grunt.registerTask('compile', ['removelogging', 'fixmyjs', 'uglify']);
 
-	grunt.registerTask("build", ["clean:build", "copy:build", "compile", "compress", "copy:demo"]);
+  grunt.registerTask('build', ['clean:build', 'copy:build', 'compile', 'compress', 'copy:demo']);
 
 };
---
+++ src/swipeoroony.js
@@ -0,0 +1,77 @@
+function initSwipe(object, parent, links, linkClass, document, window, body, transition) {
+  object.on('touchstart', function (event) {
+    if (!object.is('.galleryImageView-is-fixing')) {
+      var objectWidth = parent.width();
+      var objectPadding = parent.css('padding-left').replace('px', '') - 0;
+      var pointStartX = event.originalEvent.touches[0].pageX;
+      var pointStartY = event.originalEvent.touches[0].pageY;
+      var pointDiffX = 0;
+      var pointDiffY = 0;
+      var pointShift = 1;
+      var positionStart = object.offset().left;
+      var linkActive = links.find('.' + linkClass + '-is-active');
+      var index = links.find('.' + linkClass).index(linkActive);
+      var galleryStatus = 'middle';
+      if (linkActive.is(links.find('.' + linkClass + ':first'))) {
+        galleryStatus = 'start';
+      } else if (linkActive.is(links.find('.' + linkClass + ':last'))) {
+        galleryStatus = 'end';
+      }
+      document.on('touchmove', function (event) {
+        pointDiffX = event.originalEvent.touches[0].pageX - pointStartX;
+        pointDiffY = event.originalEvent.touches[0].pageY - pointStartY;
+        if (Math.abs(pointDiffX) > 15) {
+          pointDiffY = 0;
+          var pointDiffXMargin = pointDiffX < 0 ? 15 - objectPadding : -15 - objectPadding;
+          if (galleryStatus === 'start' && pointDiffX > 0) {
+            pointShift = 4;
+            pointDiffXMargin = -4 - objectPadding;
+          } else if (galleryStatus === 'end' && pointDiffX < 0) {
+            pointShift = 4;
+            pointDiffXMargin = 4 - objectPadding;
+          }
+          object.css(translateGallery(positionStart + pointDiffX / pointShift + pointDiffXMargin + 'px'));
+        }
+      }).on('touchend', function (event) {
+        document.off('touchmove touchend');
+        if (Math.abs(pointDiffX) > 15) {
+          if (pointDiffX > 50 && galleryStatus !== 'start') {
+            index -= 1;
+          } else if (pointDiffX < -50 && galleryStatus !== 'end') {
+            index += 1;
+          }
+          linkActive.removeClass(linkClass + '-is-active js-dotsPage-is-active');
+          links.find('.' + linkClass + ':eq(' + index + ')').addClass(linkClass + '-is-active js-dotsPage-is-active');
+          object.addClass('slides-are-fixing');
+          object.data('activeBlock', index);
+          object.trigger('gallerySwipe');
+          object.on(transition, function (event) {
+            object.removeClass('slides-are-fixing');
+            object.off(transition);
+          });
+          setTimeout(function () {
+            object.css(translateGallery(objectWidth * -index + 'px'));
+          }, 15);
+        }
+      });
+    }
+  });
+}
+function generateDots(SIZE, LIST_CLASS, PAGE_CLASS) {
+  var NAVIGATION = '<ul class="' + LIST_CLASS + ' js-dotsNavigation u-listReset">';
+  for (var index = 0; index < SIZE; index++) {
+    NAVIGATION = index === 0 ? NAVIGATION + '<li class="' + PAGE_CLASS + ' ' + PAGE_CLASS + '-is-active js-dotsPage-is-active js-dotsPage"></li>' : NAVIGATION + '<li class="' + PAGE_CLASS + ' js-dotsPage"></li>';
+  }
+  NAVIGATION += '</ul>';
+  return NAVIGATION;
+}
+function translateGallery(DISTANCE) {
+  var CSS = {
+    '-webkit-transform': 'translateX(' + DISTANCE + ') translateZ(0)',
+    '-moz-transform': 'translateX(' + DISTANCE + ') translateZ(0)',
+    '-ms-transform': 'translateX(' + DISTANCE + ')',
+    '-o-transform': 'translateX(' + DISTANCE + ')',
+    'transform': 'translateX(' + DISTANCE + ') translateZ(0)'
+  };
+  return CSS;
+}
